51c1a4c34ce1f8b6ad6c8169ccfe0234960bdc62
Sebastian Brear <ssbrear@ucdavis.edu>
2020-08-04 17:45:39 -0700

    Reorganized the files in the repo

---
+++ 6/script/script.js
@@ -0,0 +1,11 @@
+// API key for openweathermap.org
+const API_KEY = "96c82e4b7f44adda1b4bf872938f33c4";
+
+
+// Basic formatting of ajax
+$.ajax({
+    // url: 
+    method: "GET"
+}).then(function(response) {
+    //do something
+});
